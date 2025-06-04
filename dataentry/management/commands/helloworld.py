f96eb299b503b261362447d4f1574c637f57ca45
jaseel <67896327+jaseel6303@users.noreply.github.com>
2025-06-04 13:29:00 +0530

    sample custom command

---
+++ dataentry/management/commands/helloworld.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):     #   $ python manage.py helloworld --help
+    help = 'Prints Hello World'
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Hello World')
+        self.stdout.write('This is a custom management command in Django.')
+        self.stdout.write('You can run this command using: python manage.py helloworld')
