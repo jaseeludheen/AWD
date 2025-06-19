27b0463d8a6e26c25691cf0e0cb75596c5e49334
jaseel <67896327+jaseel6303@users.noreply.github.com>
2025-06-19 10:39:09 +0530

    Design a form for importing data

--- awd_main/urls.py
+++ awd_main/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from . import views
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home, name='home'), 
+    path('', include('dataentry.urls')),  # Include the URLs from the dataentry app
 ]
