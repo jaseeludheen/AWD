8800ab7bacfee8bb609c19a6f73bed64772a8718
jaseel <67896327+jaseel6303@users.noreply.github.com>
2025-06-25 15:43:00 +0530

    Handling Importdata Error

--- dataentry/management/commands/importdatafromcsv.py
+++ dataentry/management/commands/importdatafromcsv.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.apps import apps
 import csv
+from django.db import DataError
 
 
 # proposed command - python manage.py importdata file_path
@@ -36,9 +37,32 @@ class Command(BaseCommand):
         if not model:
             raise CommandError(f'Model "{model_name}" not found in any app.')
         
+        # compare csv header with model's field names
+        
+        # Get the field names from the model that we found
+#       model_fields = [field.name for field in model._meta.fields]    # id field is also included in the model fields, so we can use it to compare with the csv header
+        model_fields = [field.name for field in model._meta.fields if field.name != 'id']    # excluding the id field
+        model_fields_set = set([field.strip().lower() for field in model_fields])
+
+#       print(model_fields_set)
+
 
         with open(file_path, 'r') as file:  
-            reader = csv.DictReader(file)  
+            reader = csv.DictReader(file)  #  first row of the CSV file is considered as the header
+            # fetch the header from the csv file
+            csv_header = reader.fieldnames     # header of the csv file
+            csv_fields = set([field.strip().lower() for field in csv_header])  # strip() removes spaces like " Name " ➜ "Name".      lower() makes it lowercase, so "Name" ➜ "name". and  convert it to a set to allow unordered comparison.
+
+            # compare csv header with model's field names
+            """
+            if csv_fields != model_fields:
+                raise DataError(f"CSV file doesn't match with the {model_name} table fields.")
+            """
+            # Compare sets (case-insensitive, order-insensitive)
+            if not model_fields_set.issubset(csv_fields):
+                raise DataError(f"CSV file headers do not match the fields of the {model_name} model.\nExpected fields: {model_fields}\nCSV headers: {csv_header}")
+
+
             for row in reader:
                 model.objects.create(**row)  
         self.stdout.write(self.style.SUCCESS('Data imported from CSV successfully!'))
